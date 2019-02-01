f4b0b095311733f196939472cd8a38322510b5eb
Lucas Lacerda <lucas.lacerda@eng.ci.ufpb.br>
2019-01-31 22:45:36 -0300

    Updates

---
+++ precache-manifest.e9dddc162b554a7c88ba17b23cd626dd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "bba6fa9f0e1c4336e0cc80badf809699",
+    "url": "/meuhorario-frontend/static/media/add-course.bba6fa9f.svg"
+  },
+  {
+    "revision": "30488e9c6b9f711e1784",
+    "url": "/meuhorario-frontend/static/css/main.bab8401b.chunk.css"
+  },
+  {
+    "revision": "5bc538a9eb7184d89cb9",
+    "url": "/meuhorario-frontend/static/js/1.5bc538a9.chunk.js"
+  },
+  {
+    "revision": "081556680d5b1d3fc88f",
+    "url": "/meuhorario-frontend/static/js/runtime~main.08155668.js"
+  },
+  {
+    "revision": "41d90406ffe689ab9c295211df8b3b7f",
+    "url": "/meuhorario-frontend/static/media/print.41d90406.svg"
+  },
+  {
+    "revision": "ecdd0df17010a1effdd64460ea202f52",
+    "url": "/meuhorario-frontend/static/media/bookmark.ecdd0df1.svg"
+  },
+  {
+    "revision": "72ce94d3e31a8b14cf397453eed15309",
+    "url": "/meuhorario-frontend/static/media/link.72ce94d3.svg"
+  },
+  {
+    "revision": "73f3fdc7b0e1404eacd2a5b7aceaf7c6",
+    "url": "/meuhorario-frontend/static/media/delete.73f3fdc7.svg"
+  },
+  {
+    "revision": "30488e9c6b9f711e1784",
+    "url": "/meuhorario-frontend/static/js/main.30488e9c.chunk.js"
+  },
+  {
+    "revision": "cd9842d487b672e6c3ed60e62a698ce5",
+    "url": "/meuhorario-frontend/static/media/share.cd9842d4.svg"
+  },
+  {
+    "revision": "fea1ab47c9a82bb4068b62fa6de0ebd4",
+    "url": "/meuhorario-frontend/static/media/search.fea1ab47.svg"
+  },
+  {
+    "revision": "f5b0e3156f2793c87bd296b351ac85e9",
+    "url": "/meuhorario-frontend/static/media/messenger.f5b0e315.svg"
+  },
+  {
+    "revision": "c9b4fecd16c1c29728052d9936a94b8f",
+    "url": "/meuhorario-frontend/static/media/facebook.c9b4fecd.svg"
+  },
+  {
+    "revision": "fdd6e72441c9e6c5b1060201063324f2",
+    "url": "/meuhorario-frontend/static/media/twitter.fdd6e724.svg"
+  },
+  {
+    "revision": "a2ea4ae2695ff8c856d325aaed8db0a7",
+    "url": "/meuhorario-frontend/static/media/logo.a2ea4ae2.svg"
+  },
+  {
+    "revision": "ab5f4cce3a78d9927e08255829d0cc46",
+    "url": "/meuhorario-frontend/index.html"
+  }
+];
